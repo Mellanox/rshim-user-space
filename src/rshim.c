Mellanox/rshim-user-space

commit 90e7407605b18e08005a82eb2c889a8cb47e5777

Fix several warnings reported by coverity
Signed-off-by: Liming Sun <lsun@mellanox.com>

src/rshim.c
@@ -29,6 +29,9 @@
 /* RShim timer interval in milliseconds. */
 #define RSHIM_TIMER_INTERVAL 1
 
+/* Cycles to poll the network initialization before timeout. */
+#define RSHIM_NET_INIT_DELAY (30000 / RSHIM_TIMER_INTERVAL)
+
 /* Keepalive period in milliseconds. */
 static int rshim_keepalive_period = 300;
 
@@ -38,7 +41,7 @@ int rshim_boot_timeout = 100;
 /* Skip SW_RESET while pushing boot stream. */
 int rshim_skip_boot_reset;
 
-#define RSH_KEEPALIVE_MAGIC_NUM 0x5089836482ULL
+#define RSHIM_KEEPALIVE_MAGIC_NUM 0x5089836482ULL
 
 /* Circular buffer macros. */
 #define CIRC_SPACE(head, tail, size) CIRC_CNT((tail), ((head)+1), (size))
@@ -391,8 +394,10 @@ static ssize_t rshim_write_delayed(rshim_backend_t *bd, int devtype,
       RSHIM_ERR("write_rshim error %d\n", rc);
       break;
     }
-    buf += sizeof(reg);
     byte_cnt += sizeof(reg);
+    if (buf == pad_buf)
+      break;
+    buf += sizeof(reg);
     avail--;
   }
 
@@ -888,7 +893,7 @@ static int rshim_fifo_ctrl_tx(rshim_backend_t *bd)
 static void rshim_fifo_input(rshim_backend_t *bd)
 {
   rshim_tmfifo_msg_hdr_t *hdr;
-  bool rx_avail = false;
+  uint8_t rx_avail = 0;
   int rc;
 
   if (bd->is_boot_open)
@@ -992,7 +997,7 @@ again:
 
     if (bd->read_buf_pkt_rem <= 0) {
       bd->read_buf_next = bd->read_buf_next + bd->read_buf_pkt_padding;
-      rx_avail = true;
+      rx_avail = 1;
     }
   }
 
@@ -1026,7 +1031,7 @@ again:
   if (rx_avail && bd->rx_chan == TMFIFO_NET_CHAN) {
     if (__sync_bool_compare_and_swap(&bd->net_rx_pending, false, true)) {
       do {
-        rc = write(bd->net_notify_fd[1], &rx_avail, 1);
+        rc = write(bd->net_notify_fd[1], &rx_avail, sizeof(rx_avail));
       } while (rc == -1 && (errno == EINTR || errno == EAGAIN));
     }
   }
@@ -1415,13 +1420,15 @@ ssize_t rshim_fifo_write(rshim_backend_t *bd, const char *buffer,
 
 static void rshim_work_handler(rshim_backend_t *bd)
 {
+  int rc;
+
   pthread_mutex_lock(&bd->mutex);
 
   bd->work_pending = false;
 
   if (bd->keepalive && bd->has_rshim) {
     bd->write_rshim(bd, RSHIM_CHANNEL, RSH_SCRATCHPAD1,
-                    RSH_KEEPALIVE_MAGIC_NUM);
+                    RSHIM_KEEPALIVE_MAGIC_NUM);
     bd->keepalive = 0;
   }
 
@@ -1437,6 +1444,17 @@ static void rshim_work_handler(rshim_backend_t *bd)
   if (bd->is_boot_open) {
     pthread_mutex_unlock(&bd->mutex);
     return;
+  }
+
+  if (bd->net_fd < 0 && (rshim_timer_ticks - bd->net_init_time) <
+      RSHIM_NET_INIT_DELAY) {
+    rc = rshim_net_init(bd);
+    if (!rc) {
+      bd->is_net_open = 1;
+      pthread_mutex_lock(&bd->ringlock);
+      rshim_fifo_input(bd);
+      pthread_mutex_unlock(&bd->ringlock);
+    }
   }
 
   if (bd->has_fifo_work) {
@@ -1716,20 +1734,17 @@ int rshim_notify(rshim_backend_t *bd, int event, int code)
     if (!bd->has_reprobe && bd->has_rshim)
       rshim_fifo_sync(bd);
 
-    /* Init network interface. */
-    rc = rshim_net_init(bd);
-    if (rc < 0) {
-      RSHIM_ERR("Failed to init networking\n");
-      break;
-    }
-    bd->is_net_open = 1;
-    pthread_mutex_lock(&bd->ringlock);
-    rshim_fifo_input(bd);
-    pthread_mutex_unlock(&bd->ringlock);
+    __sync_synchronize();
+    bd->is_attach = 1;
+
+    /* Init network interface. Moved to the work handler since it takes time. */
+    bd->net_init_time = rshim_timer_ticks;
     break;
 
   case RSH_EVENT_DETACH:
     /* Shutdown network interface. */
+    __sync_synchronize();
+    bd->is_attach = 0;
     rshim_net_del(bd);
     rshim_fifo_release(bd, TMFIFO_NET_CHAN, NULL);
     break;
@@ -1881,7 +1896,8 @@ static void rshim_boot_workaround_check(rshim_backend_t *bd)
     if (!rc) {
       /* SW reset. */
       rc = rshim_reset_control(bd);
-      usleep(100000);
+      if (!rc)
+        usleep(100000);
     }
   }
 }
@@ -1920,7 +1936,7 @@ static int rshim_access_check(rshim_backend_t *bd)
       continue;
     pthread_mutex_lock(&other_bd->mutex);
     other_bd->write_rshim(other_bd, RSHIM_CHANNEL, RSH_SCRATCHPAD1,
-                    RSH_KEEPALIVE_MAGIC_NUM);
+                    RSHIM_KEEPALIVE_MAGIC_NUM);
     pthread_mutex_unlock(&other_bd->mutex);
   }
 
@@ -1934,7 +1950,7 @@ static int rshim_access_check(rshim_backend_t *bd)
     if (rc < 0)
       return -ENODEV;
 
-    if (value == RSH_KEEPALIVE_MAGIC_NUM) {
+    if (value == RSHIM_KEEPALIVE_MAGIC_NUM) {
       RSHIM_INFO("another backend already attached\n");
       return -EEXIST;
     }
@@ -2195,7 +2211,8 @@ static void rshim_main(int argc, char *argv[])
           rshim_work_handler(bd);
         continue;
       } else {
-        int index, tmp;
+        uint8_t tmp;
+        int index;
 
         /* Network. */
         for (index = 0; index < RSHIM_MAX_DEV; index++) {
@@ -2205,7 +2222,7 @@ static void rshim_main(int argc, char *argv[])
 
           if (fd == bd->net_notify_fd[0]) {
             /* Rx. */
-            if (read(fd, &tmp, 1) == 1)
+            if (read(fd, &tmp, sizeof(tmp)) == sizeof(tmp))
               rshim_net_rx(bd);
             break;
           } else if (fd == rshim_devs[index]->net_fd) {

Makefile.am
@@ -5,3 +5,9 @@
 AUTOMAKE_OPTIONS = foreign
 SUBDIRS = src man
 dist_doc_DATA = README.md
+
+AM_DISTCHECK_CONFIGURE_FLAGS = \
+  --with-systemdsystemunitdir=$$dc_install_base/$(systemdsystemunitdir)
+if HAVE_SYSTEMD
+systemdsystemunit_DATA = rshim.service
+endif

configure.ac
@@ -2,7 +2,7 @@
 # Copyright (C) 2019 Mellanox Technologies. All Rights Reserved.
 #
 
-AC_INIT([Rshim Driver for BlueField SoC], [2.0])
+AC_INIT([rshim], [2.0.1])
 AC_CONFIG_AUX_DIR(config)
 AM_INIT_AUTOMAKE([-Wall -Werror foreign])
 AC_LANG(C)
@@ -16,6 +16,21 @@ AC_CONFIG_FILES([
   rshim.spec
 ])
 AC_CANONICAL_HOST
+
+PKG_PROG_PKG_CONFIG
+AC_ARG_WITH([systemdsystemunitdir],
+ [AS_HELP_STRING([--with-systemdsystemunitdir=DIR], [Directory for systemd service files])],,
+ [with_systemdsystemunitdir=auto])
+AS_IF([test "x$with_systemdsystemunitdir" = "xyes" -o "x$with_systemdsystemunitdir" = "xauto"], [
+  def_systemdsystemunitdir=$($PKG_CONFIG --variable=systemdsystemunitdir systemd)
+  AS_IF([test "x$def_systemdsystemunitdir" = "x"],
+  [AS_IF([test "x$with_systemdsystemunitdir" = "xyes"],
+  [AC_MSG_ERROR([systemd support requested but pkg-config unable to query systemd package])])
+  with_systemdsystemunitdir=no],
+  [with_systemdsystemunitdir="$def_systemdsystemunitdir"])])
+AS_IF([test "x$with_systemdsystemunitdir" != "xno"],
+      [AC_SUBST([systemdsystemunitdir], [$with_systemdsystemunitdir])])
+AM_CONDITIONAL([HAVE_SYSTEMD], [test "x$with_systemdsystemunitdir" != "xno"])
 
 AC_ARG_ENABLE([usb],
   AS_HELP_STRING([--enable-usb], [Enable rshim over USB (default is yes) ]),

rshim.spec.in
@@ -4,7 +4,7 @@
 
 Name: rshim
 Version: @VERSION@
-Release: 2%{?dist}
+Release: 1%{?dist}
 Summary: User-space driver for Mellanox BlueField SoC
 
 License: GPLv2
@@ -12,12 +12,10 @@ License: GPLv2
 URL: https://github.com/mellanox/rshim-user-space
 Source0: https://github.com/mellanox/rshim-user-space/archive/%{name}-%{version}.tar.gz
 
-Obsoletes: %{name} < 2.0
-
 BuildRequires: gcc, autoconf, automake
 BuildRequires: pkgconfig(libpci), pkgconfig(libusb-1.0), pkgconfig(fuse)
 
-%global with_systemd %(if ( test -d "%{_unitdir}" > /dev/null); then echo -n '1'; else echo -n '0'; fi)
+%global with_systemd %(if (test -d "%{_unitdir}" > /dev/null); then echo -n '1'; else echo -n '0'; fi)
 
 %description
 This is the user-space driver to access the BlueField SoC via the rshim
@@ -38,8 +36,8 @@ make
 %install
 %makeinstall -C src INSTALL_DIR="%{buildroot}%{_bindir}"
 %if "%{with_systemd}" == "1"
-%{__install} -d %{buildroot}%{_unitdir}
-%{__install} -m 0644 rshim.service %{buildroot}%{_unitdir}
+  %{__install} -d %{buildroot}%{_unitdir}
+  %{__install} -m 0644 rshim.service %{buildroot}%{_unitdir}
 %endif
 %{__install} -d %{buildroot}%{_mandir}/man1
 %{__install} -m 0644 man/rshim.1 %{buildroot}%{_mandir}/man1
@@ -53,26 +51,29 @@ make
 
 %preun
 %if "%{with_systemd}" == "1"
-systemctl stop rshim
+  systemctl stop rshim
 %else
-killall -9 rshim
+  killall -9 rshim
 %endif
 
 %files
 %license LICENSE
 %defattr(-,root,root,-)
-%%doc README.md
+%doc README.md
 %if "%{with_systemd}" == "1"
-%{_unitdir}/rshim.service
+  %{_unitdir}/rshim.service
 %endif
 %{_bindir}/rshim
 %{_mandir}/man1/rshim.1.gz
 
 %changelog
-* Fri Mar 20 2020 Liming Sun <lsun@mellanox.com> - 2.0-2
+* Fri Mar 20 2020 Liming Sun <lsun@mellanox.com> - 2.0.1-1
 - Rename bfrshim to rshim
 - Remove rshim.spec since it's auto-generated from rshim.spec.in
+- Fix some warnings reported by coverity
+
 * Fri Mar 13 2020 Liming Sun <lsun@mellanox.com> - 2.0-1
 - Update the spec file according to fedora packaging-guidelines
+
 * Mon Dec 16 2019 Liming Sun <lsun@mellanox.com>
 - Initial packaging

src/rshim.h
@@ -192,6 +192,7 @@ struct rshim_backend {
   uint32_t is_boot_open : 1;      /* Boot device is open. */
   uint32_t is_net_open : 1;       /* Network device is open. */
   uint32_t is_cons_open : 1;      /* Console device is open. */
+  uint32_t is_attach : 1;         /* Service ready to attach. */
   uint32_t is_in_boot_write : 1;  /* A thread is in boot_write(). */
   uint32_t has_cons_work : 1;     /* Console worker thread running. */
   uint32_t has_debug : 1;         /* Debug enabled for this device. */
@@ -213,6 +214,7 @@ struct rshim_backend {
 
   /* Last keepalive time. */
   int last_keepalive;
+  int net_init_time;
 
   /* timer. */
   int timer;

src/rshim_fuse.c
@@ -47,9 +47,10 @@ char *rshim_dev_minor_names[RSH_DEV_TYPES] = {
 static void rshim_fuse_boot_open(fuse_req_t req, struct fuse_file_info *fi)
 {
   rshim_backend_t *bd = fuse_req_userdata(req);
-  int rc;
+  int rc = -ENODEV;
 
-  rc = rshim_boot_open(bd);
+  if (bd)
+    rc = rshim_boot_open(bd);
 
   if (rc)
     fuse_reply_err(req, -rc);
@@ -89,9 +90,10 @@ static void rshim_fuse_boot_write(fuse_req_t req, const char *user_buffer,
                                   struct fuse_file_info *fi)
 {
   rshim_backend_t *bd = fuse_req_userdata(req);
-  int rc;
+  int rc = -ENODEV;
 
-  rc = rshim_boot_write(bd, user_buffer, count, rshim_fuse_copy_in);
+  if (bd)
+    rc = rshim_boot_write(bd, user_buffer, count, rshim_fuse_copy_in);
 
   if (rc >= 0)
     fuse_reply_write(req, rc);
@@ -130,7 +132,9 @@ static void rshim_fuse_boot_release(fuse_req_t req, struct fuse_file_info *fi)
 {
   rshim_backend_t *bd = fuse_req_userdata(req);
 
-  rshim_boot_release(bd);
+  if (bd) 
+    rshim_boot_release(bd);
+
   fuse_reply_err(req, 0);
 }
 #elif defined(__FreeBSD__)
@@ -177,9 +181,10 @@ void rshim_fuse_input_notify(rshim_backend_t *bd)
 static void rshim_fuse_console_open(fuse_req_t req, struct fuse_file_info *fi)
 {
   rshim_backend_t *bd = fuse_req_userdata(req);
-  int rc;
+  int rc = -ENODEV;
 
-  rc = rshim_console_open(bd);
+  if (bd)
+    rc = rshim_console_open(bd);
 
   if (!rc)
     fuse_reply_open(req, fi);
@@ -211,6 +216,11 @@ static void rshim_fuse_console_read(fuse_req_t req, size_t size, off_t off,
   rshim_backend_t *bd = fuse_req_userdata(req);
   char buf[512];
   int rc;
+
+  if (!bd) {
+    fuse_reply_err(req, ENODEV);
+    return;
+  }
 
   if (off) {
     fuse_reply_err(req, EINVAL);
@@ -283,6 +293,11 @@ static void rshim_fuse_console_write(fuse_req_t req, const char *buf,
   rshim_backend_t *bd = fuse_req_userdata(req);
   int rc;
 
+  if (!bd) {
+    fuse_reply_err(req, ENODEV);
+    return;
+  }
+
   if (off) {
     fuse_reply_err(req, EINVAL);
     return;
@@ -342,9 +357,10 @@ static void rshim_fuse_console_fsync(fuse_req_t req, int datasync,
                                      struct fuse_file_info *fi)
 {
   rshim_backend_t *bd = fuse_req_userdata(req);
-  int rc;
+  int rc = -ENODEV;
 
-  rc = rshim_fifo_fsync(bd, TMFIFO_CONS_CHAN);
+  if (bd)
+    rc = rshim_fifo_fsync(bd, TMFIFO_CONS_CHAN);
 
   fuse_reply_err(req, -rc);
 }
@@ -355,6 +371,11 @@ static void rshim_fuse_console_ioctl(fuse_req_t req, int cmd, void *arg,
                                      size_t in_bufsz, size_t out_bufsz)
 {
   rshim_backend_t *bd = fuse_req_userdata(req);
+
+  if (!bd) {
+    fuse_reply_err(req, ENODEV);
+    return;
+  }
 
   pthread_mutex_lock(&bd->mutex);
 
@@ -443,6 +464,11 @@ static void rshim_fuse_console_poll(fuse_req_t req, struct fuse_file_info *fi,
   unsigned int revents = 0;
   bool poll_rx = false, poll_tx = false, poll_err = false;
 
+  if (!bd) {
+    fuse_reply_err(req, ENODEV);
+    return;
+  }
+
   rshim_fifo_check_poll(bd, TMFIFO_CONS_CHAN, &poll_rx, &poll_tx, &poll_err);
 
   if (poll_rx)
@@ -499,7 +525,9 @@ static void rshim_fuse_console_release(fuse_req_t req,
 {
   rshim_backend_t *bd = fuse_req_userdata(req);
 
-  rshim_console_release(bd, rshim_fuse_poll_handle_destroy);
+  if (bd)
+    rshim_console_release(bd, rshim_fuse_poll_handle_destroy);
+
   fuse_reply_err(req, 0);
 }
 #elif defined(__FreeBSD__)
@@ -604,6 +632,15 @@ static int rshim_fuse_misc_read(struct cuse_dev *cdev, int fflags,
   }
   rm->ready = 1;
 
+  if (!bd) {
+#ifdef __linux__
+    fuse_reply_err(req, ENODEV);
+    return;
+#elif defined(__FreeBSD__)
+    return CUSE_ERR_INVALID;
+#endif
+  }
+
   pthread_mutex_lock(&bd->mutex);
 
   /* Boot mode. */
@@ -682,11 +719,9 @@ static int rshim_fuse_misc_read(struct cuse_dev *cdev, int fflags,
     n = snprintf(p, len, "%-16s%d %d (rw)\n",
                    "VLAN_ID", bd->vlan[0], bd->vlan[1]);
     p += n;
-    len -= n;
   } else if (rshim_misc_level == 2) {
     n = rshim_log_show(bd, p, len);
     p += n;
-    len -= n;
   }
 
   rm->len = p - rm->buffer;
@@ -857,7 +892,9 @@ static void rshim_fuse_misc_release(fuse_req_t req, struct fuse_file_info *fi)
 
   free((void *)(uintptr_t)fi->fh);
   fuse_reply_err(req, 0);
-  rshim_deref(bd);
+
+  if (bd)
+    rshim_deref(bd);
 }
 #elif defined(__FreeBSD__)
 static int rshim_fuse_misc_release(struct cuse_dev *cdev, int fflags)
@@ -866,7 +903,8 @@ static int rshim_fuse_misc_release(struct cuse_dev *cdev, int fflags)
   struct rshim_misc *rm = cuse_dev_get_per_file_handle(cdev);
 
   free(rm);
-  rshim_deref(bd);
+  if (bd)
+    rshim_deref(bd);
   return CUSE_ERR_NONE;
 }
 #endif
@@ -911,6 +949,11 @@ static void rshim_fuse_rshim_ioctl(fuse_req_t req, int cmd, void *arg,
   struct iovec iov;
   uint64_t data = 0;
   int rc = 0;
+
+  if (!bd) {
+    fuse_reply_err(req, ENODEV);
+    return;
+  }
 
   switch (cmd) {
   case RSHIM_IOC_READ:

src/rshim_log.c
@@ -5,6 +5,8 @@
  */
 
 #include <pthread.h>
+#include <stdarg.h>
+#include <time.h>
 #include "rshim.h"
 
 /* Log module */
@@ -224,6 +226,17 @@ static int rshim_log_show_crash(rshim_backend_t *bd, uint64_t hdr, char *buf,
   return p - buf;
 }
 
+static int rshim_log_format_msg(char *buf, int len, const char* msg, ...)
+{
+  va_list args;
+
+  va_start(args, msg);
+  len = vsnprintf(buf, len, msg, args);
+  va_end(args);
+
+  return len;
+}
+
 static int rshim_log_show_msg(rshim_backend_t *bd, uint64_t hdr, char *buf,
                               int size)
 {
@@ -234,7 +247,7 @@ static int rshim_log_show_msg(rshim_backend_t *bd, uint64_t hdr, char *buf,
   int has_arg = BF_RSH_LOG_HEADER_GET(HAS_ARG, hdr);
   uint32_t arg = BF_RSH_LOG_HEADER_GET(ARG, hdr);
   uint64_t data;
-  char *tmp, *p;
+  char *msg, *p;
 
   if (len <= 0)
     return -EINVAL;
@@ -244,36 +257,40 @@ static int rshim_log_show_msg(rshim_backend_t *bd, uint64_t hdr, char *buf,
   if (level >= sizeof(rshim_log_levels) / sizeof(rshim_log_levels[0]))
     level = 0;
 
-  tmp = malloc(len * sizeof(uint64_t) + 1);
-  if (!tmp)
+  msg = malloc(len * sizeof(uint64_t) + 1);
+  if (!msg)
     return 0;
-  p = tmp;
+  p = msg;
 
   while (len--) {
     rc = bd->read_rshim(bd, RSHIM_CHANNEL, RSH_SCRATCH_BUF_DAT, &data);
-    if (rc)
+    if (rc) {
+      free(msg);
       return 0;
+    }
     memcpy(p, &data, sizeof(data));
     p += sizeof(data);
   }
   *p = '\0';
   if (!has_arg) {
     len = snprintf(buf, size, " %s[%s]: %s\n", rshim_log_levels[level],
-                   rshim_log_mod[module], tmp);
+                   rshim_log_mod[module], msg);
   } else {
     len = snprintf(buf, size, " %s[%s]: ", rshim_log_levels[level],
                    rshim_log_mod[module]);
-    len += snprintf(buf + len, size - len, tmp, arg);
+    // coverity[ +tainted_string_sanitize_content : arg-2 ]
+    len += rshim_log_format_msg(buf + len, size - len, msg, arg);
     len += snprintf(buf + len, size - len, "\n");
   }
 
-  free(tmp);
+  free(msg);
   return len;
 }
 
 int rshim_log_show(rshim_backend_t *bd, char *buf, int size)
 {
   uint64_t data, idx, hdr;
+  time_t t0, t1;
   int i, n, rc, type, len;
   char *p = buf;
 
@@ -288,6 +305,7 @@ int rshim_log_show(rshim_backend_t *bd, char *buf, int size)
   size -= n;
 
   /* Take the semaphore. */
+  time(&t0);
   while (true) {
     rc = bd->read_rshim(bd, RSHIM_CHANNEL, RSH_SEMAPHORE0, &data);
     if (rc) {
@@ -298,7 +316,10 @@ int rshim_log_show(rshim_backend_t *bd, char *buf, int size)
     if (!data)
       break;
 
-    usleep(10000);
+    /* Add a timeout in case the semaphore is stuck. */
+    time(&t1);
+    if (difftime(t1, t0) > 1)
+      break;
   }
 
   /* Read the current index. */

src/rshim_net.c
@@ -51,30 +51,28 @@ static int rshim_if_open(char *ifname, int index)
 {
   char cmd[128];
   struct ifreq ifr;
-  int s, fd, rc, retry;
-
-  rc = system("modprobe tun");
-  if (rc == -1)
-    RSHIM_DBG("Failed to load the tun module %m\n");
+  int s, fd, rc;
 
   fd = open("/dev/net/tun", O_RDWR);
   if (fd < 0) {
-    RSHIM_ERR("Can't open %s: %m\n", ifname);
-    return -1;
+    rc = system("modprobe tun");
+    if (rc == -1)
+      RSHIM_DBG("Failed to load the tun module %m\n");
+
+    fd = open("/dev/net/tun", O_RDWR);
+    if (fd < 0) {
+      RSHIM_ERR("Can't open %s: %m\n", ifname);
+      return -1;
+    }
   }
 
   memset(&ifr, 0, sizeof(ifr));
   ifr.ifr_flags = IFF_TAP | IFF_NO_PI;
-  strncpy(ifr.ifr_name, ifname, IFNAMSIZ);
+  strncpy(ifr.ifr_name, ifname, sizeof(ifr.ifr_name) - 1);
 
-  retry = 16;
-  do {
-    rc = ioctl(fd, TUNSETIFF, (void *) &ifr);
-    if (rc == -1 && retry)
-      sleep(1);
-  } while (rc == -1 && errno == EBUSY && retry--);
+  rc = ioctl(fd, TUNSETIFF, (void *) &ifr);
   if (rc < 0) {
-    RSHIM_ERR("ioctl failed: %m, errno=%d\n", errno);
+    RSHIM_ERR("ioctl failed: errno=%d\n", errno);
     close(fd);
     return -1;
   }
@@ -102,9 +100,11 @@ static int rshim_if_open(char *ifname, int index)
   close(s);
 
   rshim_if_set_non_blocking(fd);
+
   sprintf(cmd, "ifup %s 2>/dev/null&", ifname);
   if (system(cmd) == -1)
     RSHIM_DBG("Failed to call ifup\n");
+
   return fd;
 }
 #elif defined(__FreeBSD__)
@@ -212,19 +212,27 @@ static int rshim_if_write(int fd, const char *buf, size_t len)
 #ifdef __linux__
 static void rshim_if_close(int fd)
 {
-  if (fd >= 0) {
-    ioctl(fd, TUNSETPERSIST, 0);
-    close(fd);
+  struct ifreq ifr;
+  char cmd[128];
+  int rc;
+
+  memset(&ifr, 0, sizeof(ifr));
+  rc = ioctl(fd, TUNGETIFF, (void *) &ifr);
+
+  if (!rc && ifr.ifr_name[0]) {
+    sprintf(cmd, "ifdown %s 2>/dev/null&", ifr.ifr_name);
+    if (system(cmd) == -1)
+      RSHIM_DBG("Failed to call ifdown\n");
   }
+
+  ioctl(fd, TUNSETPERSIST, 0);
+  close(fd);
 }
 #elif defined(__FreeBSD__)
 static void rshim_if_close(int fd)
 {
   struct ifreq ifr;
   int s;
-
-  if (fd < 0)
-    return;
 
   memset(&ifr, 0, sizeof(ifr));
   if (ioctl(fd, TAPGIFNAME, &ifr) < 0) {
@@ -253,8 +261,8 @@ static void rshim_if_close(int fd)
 int rshim_net_init(rshim_backend_t *bd)
 {
   struct epoll_event event;
-  char ifname[64];
-  int rc;
+  char ifname[IFNAMSIZ];
+  int rc, fd[2];
 
   snprintf(ifname, sizeof(ifname), "tmfifo_net%d",
            bd->index + rshim_index_base);
@@ -269,47 +277,54 @@ int rshim_net_init(rshim_backend_t *bd)
   event.events = EPOLLIN;
   rc = epoll_ctl(rshim_epoll_fd, EPOLL_CTL_ADD, bd->net_fd, &event);
   if (rc == -1) {
-    RSHIM_ERR("epoll_ctl failed: %m %d %d\n", rshim_epoll_fd, bd->net_fd);
-    return rc;
+    RSHIM_ERR("epoll_ctl failed: %d %d\n", rshim_epoll_fd, bd->net_fd);
+    goto fail;
   }
 
-  rc = pipe(bd->net_notify_fd);
+  rc = pipe(fd);
   if (rc == -1) {
     perror("Failed to create pipe %m");
-    return rc;
+    goto fail;
   }
 
-  event.data.fd = bd->net_notify_fd[0];
+  event.data.fd = fd[0];
   event.events = EPOLLIN;
-  rc = epoll_ctl(rshim_epoll_fd, EPOLL_CTL_ADD, bd->net_notify_fd[0], &event);
+  rc = epoll_ctl(rshim_epoll_fd, EPOLL_CTL_ADD, fd[0], &event);
   if (rc == -1) {
-    RSHIM_ERR("epoll_ctl failed: %m %d %d\n",
-              rshim_epoll_fd, bd->net_notify_fd[0]);
-    bd->net_notify_fd[0] = -1;
-    return rc;
+    RSHIM_ERR("epoll_ctl failed: %d %d\n", rshim_epoll_fd, fd[0]);
+    goto fail;
   }
+  bd->net_notify_fd[0] = fd[0];
+  bd->net_notify_fd[1] = fd[1];
 
   return 0;
+fail:
+  rshim_if_close(bd->net_fd);
+  bd->net_fd = -1;
+  return rc;
 }
 
 int rshim_net_del(rshim_backend_t *bd)
 {
   struct epoll_event event;
 
-  memset(&event, 0, sizeof(event));
-
-  if (bd->net_fd >= 0) {
-    event.data.fd = bd->net_fd;
-    epoll_ctl(rshim_epoll_fd, EPOLL_CTL_DEL, bd->net_fd, &event);
-  }
-
   if (bd->net_notify_fd[0] >= 0) {
+    memset(&event, 0, sizeof(event));
     event.data.fd = bd->net_notify_fd[0];
     epoll_ctl(rshim_epoll_fd, EPOLL_CTL_DEL, bd->net_notify_fd[0], &event);
+    close(bd->net_notify_fd[0]);
+    close(bd->net_notify_fd[1]);
+    bd->net_notify_fd[0] = -1;
+    bd->net_notify_fd[1] = -1;
   }
 
-  rshim_if_close(bd->net_fd);
-  bd->net_fd = -1;
+  if (bd->net_fd >= 0) {
+    memset(&event, 0, sizeof(event));
+    event.data.fd = bd->net_fd;
+    epoll_ctl(rshim_epoll_fd, EPOLL_CTL_DEL, bd->net_fd, &event);
+    rshim_if_close(bd->net_fd);
+    bd->net_fd = -1;
+  }
   return 0;
 }
 

commit c0494d4d9eb123535e6151ab89a242f58e5ee95f

Fix network interface missing issue when pushing boot stream
This commit fixes an issue which was introduced by recent commit
90e7407605b1 to set bringup the network interface with a timeout
value.

Signed-off-by: Liming Sun <lsun@mellanox.com>

src/rshim.c
@@ -1441,11 +1441,6 @@ static void rshim_work_handler(rshim_backend_t *bd)
     pthread_cond_broadcast(&bd->boot_write_complete_cond);
   }
 
-  if (bd->is_boot_open) {
-    pthread_mutex_unlock(&bd->mutex);
-    return;
-  }
-
   if (bd->net_fd < 0 && (rshim_timer_ticks - bd->net_init_time) <
       RSHIM_NET_INIT_DELAY) {
     rc = rshim_net_init(bd);
@@ -1455,6 +1450,11 @@ static void rshim_work_handler(rshim_backend_t *bd)
       rshim_fifo_input(bd);
       pthread_mutex_unlock(&bd->ringlock);
     }
+  }
+
+  if (bd->is_boot_open) {
+    pthread_mutex_unlock(&bd->mutex);
+    return;
   }
 
   if (bd->has_fifo_work) {